63cf2a07b8c0ecb276e09d87696e8d04966bad2c
sawankumar-pal-au9 <manojpal211999@gmail.com>
2024-10-16 12:38:37 +0530

    added the roman no program in solvings

--- int/solving.js
+++ int/solving.js
@@ -230,3 +230,41 @@ const array = [[2, 4], [6, 4], [6, 5]]
 //  }
 
 //  console.log(sum)
+
+
+/////////////////////////////////////////////////////
+
+// function romanToInt(s) {
+//     const romanMap = {
+//         'I': 1,
+//         'V': 5,
+//         'X': 10,
+//         'L': 50,
+//         'C': 100,
+//         'D': 500,
+//         'M': 1000
+//     };
+
+//     let total = 0;
+//     let prevValue = 0;
+
+//     for (let i = s.length - 1; i >= 0; i--) {
+//         const currentValue = romanMap[s[i]];
+        
+//         if (currentValue < prevValue) {
+//             total -= currentValue;
+//         } else {
+//             total += currentValue;
+//         }
+        
+//         prevValue = currentValue;
+//     }
+
+//     return total;
+// }
+
+// console.log(romanToInt("III"));    
+// console.log(romanToInt("IV"));     
+// console.log(romanToInt("IX"));     
+// console.log(romanToInt("LVIII")); 
+// console.log(romanToInt("MCMXCIV")); 
